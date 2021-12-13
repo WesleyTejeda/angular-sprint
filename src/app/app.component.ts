6a54f0e34372ed5cda77bafc876d020e6890d965
Wesley Tejeda <wtejeda@talentpath.com>
2021-12-13 17:45:23 -0500

    Fixed api calls, commented out docheck and changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnChanges, SimpleChanges } from '@angular/core';
 import { GetService } from './services/get.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-sprint';
   symbol:string = '';
+  symbolBank:any= {
+    result: [
+      {
+        description: '',
+        displaySymbol: '',
+        symbol: '',
+        type: ''}
+      ]
+  };
+  suggested:any = [];
   // private url = `https://finnhub.io/api/v1/search?q=${this.symbol}&token=c0mtnhf48v6tkq13l9bg`;
+
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   console.log(changes);
+  //   console.log("changing");
+  //     this.filterResults(this.symbol);
+  // }
+
+  // ngDoCheck(): void {
+  //   if(this.symbol.length >= 2){
+  //     this.suggested = this.filterResults(this.symbol);
+  //   }
+  //   else {
+
+  //   }
+  // }
+
+  ngOnInit(): void {
+      this.getService.getSymbols().subscribe(data => {
+        this.symbolBank = data;
+        console.log(this.symbolBank);
+      })
+  }
 
   constructor(private getService: GetService){}
   getData(){
-    console.log(this.symbol);
+    // console.log(this.symbol);
     console.log(this.getService.getPosts(this.symbol).subscribe(data => {
       console.log(data);
     }));
   }
+
+  // filterResults(symbol:string){
+  //   // console.log(symbol);
+  //   return this.symbolBank.result.filter((data:any) => {
+  //     if(data.symbol.includes(symbol)){
+  //       console.log(data)
+  //       return data
+  //     }
+  //   });
+  // }
 }
--- src/app/services/get.service.ts
+++ src/app/services/get.service.ts
@@ -8,6 +8,7 @@ export class GetService {
   // results:any = [];
   query:string = '';
   private url:string = 'https://finnhub.io/api/v1/search?q=';
+  private symbolUrl:string = 'https://finnhub.io/api/v1/search?q=&token=c0mtnhf48v6tkq13l9bg'
   private key:string = '&token=c0mtnhf48v6tkq13l9bg';
 
   constructor(private httpClient: HttpClient) { }
@@ -16,5 +17,9 @@ export class GetService {
     this.query = search;
     console.log(this.url+this.query+this.key);
     return this.httpClient.get(this.url+this.query+this.key);
+  }
+
+  getSymbols(){
+    return this.httpClient.get(this.symbolUrl);
   }
 }
