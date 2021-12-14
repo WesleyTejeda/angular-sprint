d033474180ead46a77099607a16d327dcb4efd97
Wesley Tejeda <wtejeda@talentpath.com>
2021-12-14 10:48:57 -0500

    Updated api data to reflect on html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, DoCheck, OnChanges, SimpleChanges } from '@angular/core';
 import { GetService } from './services/get.service';
 import { OnInit } from '@angular/core';
+import { query } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +11,25 @@ import { OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'angular-sprint';
   symbol:string = '';
-  symbolBank:any= {
-    result: [
-      {
-        description: '',
-        displaySymbol: '',
-        symbol: '',
-        type: ''}
-      ]
+  // symbolBank:any = [{}];
+  // reducedBank:any = [];
+  // suggested:any = [];
+  result:any = [];
+  news:any = [{}];
+  companyProfile:any = {
+    country: "",
+    currency: "",
+    exchange: "",
+    finnhubIndustry: "",
+    ipo: "",
+    logo: "",
+    marketCapitalization: 0,
+    name: "",
+    phone: "",
+    shareOutstanding: 0,
+    ticker: "",
+    weburl: ""
   };
-  suggested:any = [];
   // private url = `https://finnhub.io/api/v1/search?q=${this.symbol}&token=c0mtnhf48v6tkq13l9bg`;
 
   // ngOnChanges(changes: SimpleChanges): void {
@@ -31,34 +41,62 @@ export class AppComponent implements OnInit {
   // ngDoCheck(): void {
   //   if(this.symbol.length >= 2){
   //     this.suggested = this.filterResults(this.symbol);
-  //   }
-  //   else {
-
+  //     console.log(this.suggested, "suggested array");
+  //     console.log(this.symbol)
   //   }
   // }
 
   ngOnInit(): void {
-      this.getService.getSymbols().subscribe(data => {
-        this.symbolBank = data;
-        console.log(this.symbolBank);
-      })
+      // this.getService.getSymbols().subscribe(data => {
+      //   this.symbolBank = data;
+      //   console.log(this.symbolBank);
+      // })
+      this.getNews();
   }
 
   constructor(private getService: GetService){}
   getData(){
     // console.log(this.symbol);
-    console.log(this.getService.getPosts(this.symbol).subscribe(data => {
-      console.log(data);
-    }));
+    this.getService.getPosts(this.symbol).subscribe((data:any) => {
+      this.result = data.result[0];
+      console.log(this.result);
+      this.getCompanyInfo(this.result.symbol);
+    });
   }
 
   // filterResults(symbol:string){
   //   // console.log(symbol);
   //   return this.symbolBank.result.filter((data:any) => {
   //     if(data.symbol.includes(symbol)){
-  //       console.log(data)
+  //       // console.log(data)
   //       return data
   //     }
   //   });
   // }
+
+  getNews(){
+    this.getService.getNews().subscribe(data => {
+      this.news = data;
+      this.news.forEach((listing:any) => {
+        if(listing.datetime){
+          listing.datetime = this.convertUnix(listing.datetime);
+        }
+      });
+      console.log(this.news);
+    })
+  }
+
+  convertUnix(unixStamp:number){
+    let time = new Date(unixStamp * 1000);
+    return time.toLocaleString();
+  }
+
+  getCompanyInfo(companySymbol:string){
+    if(query.length != 0){
+      this.getService.getCompany(companySymbol).subscribe(data => {
+        this.companyProfile = data;
+        console.log(this.companyProfile);
+      })
+    }
+  }
 }
--- src/app/services/get.service.ts
+++ src/app/services/get.service.ts
@@ -10,6 +10,8 @@ export class GetService {
   private url:string = 'https://finnhub.io/api/v1/search?q=';
   private symbolUrl:string = 'https://finnhub.io/api/v1/search?q=&token=c0mtnhf48v6tkq13l9bg'
   private key:string = '&token=c0mtnhf48v6tkq13l9bg';
+  private newsUrl = 'https://finnhub.io/api/v1/news?category=general&token=c0mtnhf48v6tkq13l9bg';
+  private companyUrl = 'https://finnhub.io/api/v1/stock/profile2?symbol='
 
   constructor(private httpClient: HttpClient) { }
 
@@ -18,8 +20,16 @@ export class GetService {
     console.log(this.url+this.query+this.key);
     return this.httpClient.get(this.url+this.query+this.key);
   }
+  
+  getNews(){
+    return this.httpClient.get(this.newsUrl);
+  }
 
-  getSymbols(){
-    return this.httpClient.get(this.symbolUrl);
+  // getSymbols(){
+  //   return this.httpClient.get(this.symbolUrl);
+  // }
+
+  getCompany(companySymbol:string){
+    return this.httpClient.get(this.companyUrl+companySymbol+this.key);
   }
 }
